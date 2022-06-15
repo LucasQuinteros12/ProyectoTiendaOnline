1e8abefddb5fcd68ce6b7f511e53088c669fedfc
Lucas Quinteros <lucas_quinteros03@hotmail.com>
2022-06-15 15:01:42 -0300

    Buscador Incorporado

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
 const productos = [
   {id:1, nombre: "Auricular Marvo", precio: 4000},
   {id:2, nombre: "Bicicleta Venzo", precio: 40000},
-  {id:3, nombre: "Teclado Kumara", precio: 52000},
+  {id:3, nombre: "Teclado Kumara", precio: 5200},
   {id:4, nombre: "Monitor 32' ", precio: 18000},
   {id:5, nombre: "Mouse Invader", precio: 2500},
   {id:6, nombre: "Silla Gamer", precio: 50000}
 
 ];
 
-for (const producto of productos) {
-  let contenedor = document.createElement("div");
+const formulario = document.querySelector('#formulario');
+const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultado');
 
-  contenedor.innerHTML = `<div class="container">
-                            <img src="/img/">
-                            <h3>ID: ${producto.id}</h3>
-                            <p> Producto: ${producto.nombre}</p>
-                            <b> $${producto.precio}</b>
-                          </div>`;
-  document.body.appendChild(contenedor);
-}
+const filtrar = () => {
+  
+  resultado.innerHTML = '';
+
+  const texto = formulario.value.toLowerCase();
+
+  for(let producto of productos){
+
+    let nombre = producto.nombre.toLowerCase();
+
+    if(nombre.indexOf(texto) !== -1){
+      resultado.innerHTML += 
+      
+      `<li>${producto.nombre} - $${producto.precio}</li>`
+      
+    }
+  }
+
+  if(resultado.innerHTML === ''){
+    resultado.innerHTML += `
+      <h5>Producto no encontrado...</h5>
+    `
+  }
+
+};
+
+boton.addEventListener('click', filtrar)
+formulario.addEventListener('keyup', filtrar)
+
