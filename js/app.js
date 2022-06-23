fb54f4da257c6d170f3e23ae0e347e7d97b58d88
Lucas Quinteros <lucas_quinteros03@hotmail.com>
2022-06-22 22:20:04 -0300

    carrito de compras,formulario inicio sesion

--- js/app.js
+++ js/app.js
@@ -14,9 +14,16 @@ const buscador = document.getElementById('search');
 
 //buscador 
 
-mostrarProductos();
+buscador.addEventListener('input', (e)=>{
+    console.log(e.target.value)
+    let busca = stockProductos.filter(el => el.nombre.toLowerCase().includes(e.target.value))
+    mostrarProductos(busca)
+})
 
-contenedorProductos.innerHTML = '<img class= "loading"src="https://www.hermex.es/img/shopping.gif">'
+
+//loading
+
+contenedorProductos.innerHTML = '<img class= "loading"src="https://i.gifer.com/T04A.gif">'
 
 setTimeout(() => {
     mostrarProductos(stockProductos)
@@ -26,6 +33,7 @@ setTimeout(() => {
 //Logica
 
 function mostrarProductos(){
+    contenedorProductos.innerHTML= ""
     stockProductos.forEach(el=> {
 
         let div = document.createElement('div')
@@ -34,7 +42,7 @@ function mostrarProductos(){
                             <div class="card-image">
                                 <img src="${el.img}">
                                 <span class="card-title">${el.nombre}</span>
-                                <a id="boton${el.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                                <a id="boton${el.id}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="fa-solid fa-cart-arrow-down material-icons"></i></a>
                             </div>
                             <div class="card-content">
                                 <p>${el.desc}</p>
@@ -43,28 +51,58 @@ function mostrarProductos(){
                         </div>`
         
         contenedorProductos.appendChild(div)
-       
+        let btnAgregar = document.getElementById(`boton${el.id}`)
+        btnAgregar.addEventListener('click', ()=>{
+            agregarAlCarrito(el.id)
+        })
     })
 
     
 }
 
-function agregarAlCarrito(){
-
-
+function agregarAlCarrito(id){
+    let productoAgregar = stockProductos.find(item => item.id === id)
+    carritoDeCompras.push(productoAgregar)
+    mostrarCarrito(productoAgregar)
+    actualizarCarrito()
+    
+    localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
 }
 
-function mostrarCarrito(){
+function mostrarCarrito(productoAgregar){
 
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML= `<p>${productoAgregar.nombre}</p>
+                    <p>Precio: $${productoAgregar.precio}</p>
+                    <button id=eliminar${productoAgregar.id} class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+    contenedorCarrito.appendChild(div)
 
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        btnEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter(ele => ele.id !== productoAgregar.id)
+        actualizarCarrito()
+        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras))
+    })
 }
 
 function actualizarCarrito(){
-
-
+    contadorCarrito.innerText = carritoDeCompras.length
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
 }
 
 function recuperar(){
-
-
+    
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    
+    if(recuperarLS){
+        for (const elemento of recuperarLS) {
+            mostrarCarrito(elemento)
+            carritoDeCompras.push(elemento)
+            actualizarCarrito()
+        }
+    }
 }
+
+recuperar();
